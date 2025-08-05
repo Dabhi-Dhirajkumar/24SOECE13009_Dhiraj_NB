14a66abb9fb3c0ec0fbc5c2ee829ca110dfb2694
Dabhi-Dhirajkumar <dhirajjdabhi1@gmail.com>
2025-08-05 11:42:14 +0530

    constructor

---
+++ constructor.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13009_Dhiraj_NB
+{
+    internal class student
+    {
+        int rlno;
+        string name;
+
+        //default constructor
+        public student()
+        {
+            Console.WriteLine("Enter rollnumber");
+            rlno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter name");
+            name = Console.ReadLine();
+
+        }
+
+        //parameterized constructor
+        public student(int rlno, string name)
+        {
+            this.rlno = rlno;
+            this.name = name;
+        }
+
+        //copy constructor
+        public student(student s)
+        {
+            rlno = s.rlno;
+            name = s.name;
+        }
+
+        //static constructor
+        static student()
+        {
+            Console.WriteLine("Static Constructor Called");
+        }
+
+        public void showData()
+        {
+            Console.WriteLine("Roll No: " + rlno);
+            Console.WriteLine("Name: " + name);
+        }
+    }
+
+    internal class Democlass
+    {
+        public static void Main(string[] args)
+        {
+            // Using default constructor
+            student s1 = new student();
+            s1.showData();
+
+            // Using default constructor again to show that it can be reused
+            student s2 = new student();
+            s2.showData();
+
+
+            // Using parameterized constructor
+            student s3 = new student(110, "dhiraj");
+            s3.showData();
+
+            int rollNo = 101;
+            string studentName = "Dhiraj Kumar";
+
+            // Using parameterized constructor with different values With initialze the  valriable
+            student s4 = new student(rollNo, studentName);
+            s4.showData();
+
+            // Using copy constructor
+            student s5 = new student(s2);
+            s5.showData();
+        }
+    }
+
+    public class Student
+    {
+        //object initilize with array
+        public static void Main(string[] args)
+        {
+           student[]s = new student[2];
+            for (int i= 0;i<s.Length;  i++)
+            {
+                s[i] = new student();
+            }
+
+            Console.WriteLine("Details of Students:");
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine("Student " + (i + 1) + " details:");
+                s[i].showData();
+            }
+
+        }
+    }
+}
+
---
+++ constructor_exercise.cs
@@ -0,0 +1,99 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Xml.Linq;
+
+//namespace _24SOECE13009_Dhiraj_NB
+//{
+
+//    internal class Book
+//    {
+//        //default constructor
+
+//        int Bid;
+//        string Btitle;
+//        string Bauthor;
+//        double Bprice;
+//        public Book()
+//        {
+//            Console.WriteLine("Enter bookid");
+//            Bid = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Enter booktitle");
+//            Btitle = Console.ReadLine();
+//            Console.WriteLine("Enter bookauthor");
+//            Bauthor = Console.ReadLine();
+//            Console.WriteLine("Enter bookprice");
+//            Bprice = Convert.ToInt32(Console.ReadLine());
+//        }
+
+//        public void showData()
+//        {
+//            Console.WriteLine("Enter bookid " + Bid);
+//            Console.WriteLine("Enter booktitle " + Btitle);
+//            Console.WriteLine("Enter bookauthor" + Bauthor);
+//            Console.WriteLine("Enter bookprice" + Bprice);
+//        }
+//    }
+//    public class constructor_exercise
+//    {
+//        //object initilize with array
+//        public static void Main(string[] args)
+//        {
+//            constructor_exercise[] s = new constructor_exercise[2];
+//            for (int i = 0; i < s.Length; i++)
+//            {
+//                s[i] = new constructor_exercise();
+//            }
+
+//            Console.WriteLine("Details of books:");
+//            for (int i = 0; i < s.Length; i++)
+//            {
+//                Console.WriteLine("boks " + (i + 1) + " details:");
+//                s[i].showData();
+//            }
+
+//        }
+//    }
+//}
+
+using System;
+
+namespace _24SOECE13009_Dhiraj_NB
+{
+    public class Employee1
+    {
+        int eID;
+        string eName;
+        double eSalary;
+
+        // Constructor that accepts parameters
+        public Employee1(int id, string name, double salary)
+        {
+            eID = id;
+            eName = name;
+            eSalary = salary;
+        }
+
+        public void showData()
+        {
+            Console.WriteLine("Employee ID is: " + eID);
+            Console.WriteLine("Employee Name is: " + eName);
+            Console.WriteLine("Employee Salary is: " + eSalary);
+        }
+    }
+
+    internal class constructor_exercise
+    {
+        public static void Main(string[] args)
+        {
+
+            int id = int.Parse(args[0]);
+            string name = args[1];
+            string salary = args[2];
+
+            Console.WriteLine();
+        }
+    }
+}
